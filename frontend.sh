28ce635cf544c6d02c1607a48cb75eb4d35dbf8c
meghasyam1997 <meghasyam01@gmail.com>
2024-05-24 20:40:48 +0530

    frontend script

---
+++ frontend.sh
@@ -0,0 +1,22 @@
+echo -e "\e[32m>>>>>>> Install Nginx <<<<<<<<<<<\e[0m"
+dnf install nginx -y
+
+echo -e "\e[32m>>>>>>> Enable Nginx <<<<<<<<<<<\e[0m"
+systemctl enable nginx
+systemctl restart nginx
+
+echo -e "\e[32m>>>>>>> Remove the App Directory<<<<<<<<<<<\e[0m"
+rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[32m>>>>>>> Download the app content <<<<<<<<<<<\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[32m>>>>>>> Unzip the Frontend <<<<<<<<<<<\e[0m"
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+echo -e "\e[32m>>>>>>> Copy the Roboshop Configuration <<<<<<<<<<<\e[0m"
+cp /home/centos/shell-roboshop/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[32m>>>>>>> Restart Nginx <<<<<<<<<<<\e[0m"
+systemctl restart nginx
